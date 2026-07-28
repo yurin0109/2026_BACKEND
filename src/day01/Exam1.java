797b9f91588b1b8c8fc6b1c173eff1ad053f2917
yurin0109 <yurin0109>
2026-07-28 11:14:45 +0900

    26.07.28

--- src/day01/Exam1.java
+++ src/day01/Exam1.java
@@ -1,6 +1,6 @@
 package day01; // 패키지(폴더명) , 클래스 위치 식별 용도
 
-public class Exam {
+public class Exam1 {
     // public : 공개용 , 다른 패키지에서도 사용 가능한 클래스 뜻
     // class : 클래스 선언 사용하는 키워드
     // Exam : 클래스명은 아무거나 , 첫글자는 대문자!!
@@ -12,7 +12,7 @@ public class Exam {
 
     // [1] 자바 실행 시작점 생성 = main함수( 번역한 코드를 읽어들이는 흐름 단위 = 메인스레드 )
     // m + 엔터 : main{ } 자동완성 , JS에 없는 이유는 브라우저 엔진 대신한다.
-    int a = 10;
+    
     public static void main(String[] args) { 
         // ** [ main함수 안 ] 선언/만들기 . 실행문 가능 **
         // so + 엔터 : System.out.println();
