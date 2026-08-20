9b0af5cb7d77506a876d1fb27d8c54d7edd8e995
yurin0109 <yurin0109>
2026-08-20 23:38:24 +0900

    2026.08.20

--- src/day12/pa2ting3.java
+++ src/day12/pa2ting3.java
@@ -5,29 +5,33 @@ import java.util.Scanner;
 public class pa2ting3 {
     public static void main(String[] args) {
 
-    String[] contents = new String[100];
-    String[] writers = new String[100];
+        String[] contents = new String[100];
+        String[] writers = new String[100];
 
-    Scanner scan = new Scanner(System.in);
-
-    for( ; ; ){
-        System.out.println("===== My Community =====");
-        System.out.println("1. 게시물 작성 2. 게시물 출력");
-        System.out.println(" > 선택 ");
-        int ch = scan.nextInt();
-        
-        if( ch == 1 ){
-            System.out.print("내용: "); String content = scan.next();
-            System.out.print("작성자: "); String writer = scan.next();
-            boolean save = false;
-            for(int index = 0; index <= contents.length-1; index++ ){
-                if(contents[index] == null && writers[index] == null ){
-                    contents[index] = content; writers[index] = null;
-                    save = true; break;
+        Scanner scan = new Scanner(System.in);
+        for( ; ; ){
+            System.out.println(" == my community == ");
+            System.out.println(" 1. 게시물 작성 2. 게시물 출력 ");
+            System.out.println(" >선택 ");
+            int ch = scan.nextInt();
+            if( ch == 1 ){
+                System.out.print(" 내용: "); String content = scan.next();
+                System.out.print(" 작성자: "); String writer = scan.next();
+                boolean save = false;
+                for( int index = 0; index <= contents.length-1; index++ ){
+                    if(contents[index] == null && writers[index] == null ){
+                        contents[index] = content; writers[index] = writer;
+                        save = true; break;
+                    }
                 }
-            } // for end
-            if( save == true ){System.out.println("[안내] 글쓰기 성공 ");}
-
-        }
-
-    }
+                if( save == true ){System.out.println("게시물 작성 성공");}
+                else{System.out.println("게시물 작성 실패");}
+            }else if(ch == 2 ){
+                for( int index = 0; index <= contents.length-1; index++ )
+                    if( contents[index] != null && writers[index] != null ){
+                        System.out.printf(" 내용 : %s , 작성자 : %s \n " , contents[index] , writers[index] );
+                    } // if
+                } // else if
+            } // if
+        } // main
+    } // class
